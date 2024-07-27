ea8f21fc3366003428e463787dd9a602a02d547e
Roberto Montero <r@rob.cr>
2024-07-27 09:30:48 -0600

    Enable analytics and insights.
    Signed-off-by: Roberto Montero <r@rob.cr>

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,6 +55,8 @@ export default function RootLayout({ children }) {
           )}
           {children}
           <PrismicPreview repositoryName={repositoryName} />
+          <SpeedInsights />
+          <Analytics />
         </main>
       </body>
     </html>
